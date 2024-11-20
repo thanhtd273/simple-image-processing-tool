1f338da1220676f53465dfbee42e3474bf3f5db3
kiet1107 <kiettran813@gmail.com>
2024-11-20 22:48:25 +0700

    Edit entire GUI and add RemoveBG function

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import tkinter as tk
 from tkinter import filedialog
-from PIL import Image, ImageTk, ImageEnhance, ImageOps
+from tkinter.font import Font
+from PIL import Image, ImageTk, ImageEnhance, ImageOps, ImageFilter
+import numpy as np
+import cv2
 import os
+
 
 # Global variables to store images
 original_image = None
-filtered_image = None
-
+edited_image = None
 def open_image():
     global original_image
     file_path = filedialog.askopenfilename(
@@ -14,32 +17,8 @@ def open_image():
     if file_path:
         # Open the image
         original_image = Image.open(file_path)
-        
         # Display in the original image panel
-        display_image(original_image, original_label)
-
-        # Display only the file name in the status label
-        file_name = os.path.basename(file_path)
-        status_label.config(text=f"Loaded: {file_name}")
-
-def apply_filter():
-    global original_image, filtered_image
-    if original_image:
-        # Get the selected filter
-        selected_filter = filter_var.get()
-        filtered_image = original_image.copy()
-
-        # Apply the selected filter
-        if selected_filter == "Grayscale":
-            filtered_image = ImageOps.grayscale(filtered_image)
-        elif selected_filter == "Sepia":
-            sepia_image = ImageEnhance.Color(filtered_image).enhance(0.3)
-            filtered_image = ImageOps.colorize(sepia_image.convert("L"), "#704214", "#C0A080")
-        elif selected_filter == "Invert":
-            filtered_image = ImageOps.invert(filtered_image.convert("RGB"))
-
-        # Display in the filtered image panel
-        display_image(filtered_image, filtered_label)
+        display_image(original_image, i_label)
 
 def display_image(image, label):
     """Utility function to display an image in a specified label."""
@@ -49,51 +28,127 @@ def display_image(image, label):
     label.config(image=img_tk)
     label.image = img_tk
 
+def apply_filter():
+    global original_image, edited_image
+    if original_image:
+        # Get the selected filter
+        selected_filter = filter_var.get()
+        edited_image = original_image.copy()
+
+        # Apply the selected filter
+        if selected_filter == "Grayscale":
+            edited_image = ImageOps.grayscale(edited_image)
+        elif selected_filter == "Sepia":
+            sepia_image = ImageEnhance.Color(edited_image).enhance(0.3)
+            edited_image = ImageOps.colorize(sepia_image.convert("L"), "#704214", "#C0A080")
+        elif selected_filter == "Invert":
+            edited_image = ImageOps.invert(edited_image.convert("RGB"))
+        elif selected_filter == "Gaussian":
+            #edited_image = Image.fromarray(cv2.GaussianBlur(np.array(edited_image), (5, 5), 2))
+            edited_image = gaussian_filter(np.array(edited_image), sigma=2, radius=2)
+        elif selected_filter == "Median":
+            edited_image = Image.fromarray(cv2.medianBlur(np.array(edited_image), 5))
+            
+
+
+def remove_background():
+    global original_image, edited_image
+    if original_image:
+        # Get the selected filter
+        selected_threshold = rm_var.get()
+        edited_image = original_image.copy()
+        # Remove BG
+        if selected_threshold == "50":
+            edited_image = Image.fromarray(np.where(cv2.GaussianBlur(np.array(edited_image), (5, 5), 2) > 50, 50, cv2.GaussianBlur(np.array(edited_image), (5, 5), 2)))
+        elif selected_threshold == "100":
+            edited_image = Image.fromarray(np.where(cv2.GaussianBlur(np.array(edited_image), (5, 5), 2) > 100, 100, cv2.GaussianBlur(np.array(edited_image), (5, 5), 2)))
+        elif selected_threshold == "150":
+            edited_image = Image.fromarray(np.where(cv2.GaussianBlur(np.array(edited_image), (5, 5), 2) > 150, 150, cv2.GaussianBlur(np.array(edited_image), (5, 5), 2)))
+
 # Create the main application window
 root = tk.Tk()
-root.title("Image Processing Application")
+root.title("Simple photoshop")
 root.geometry("850x500")
 
 # Create a split layout using frames
-frame = tk.Frame(root)
+frame = tk.Frame(root, bg="lightgrey")
 frame.pack(fill="both", expand=True)
 
+# Label Name
+filter_label = tk.Label(frame, text="Choose an action:", bg="lightgrey", font=Font(family="Times New Roman", size=11,weight="bold", slant="italic", underline=1))
+filter_label.place(x=0,y=4)
+
+# Label Filter
+filter_label = tk.Label(frame, text="Filter", bg="lightgrey", font=("Times New Roman", 10))
+filter_label.place(x=125,y=7)
+
+# Dropdown menu Filters
+filter_var = tk.StringVar(value="None")
+filter_menu = tk.OptionMenu(frame, filter_var, "Grayscale", "Sepia", "Invert", "Gaussian", "Median")
+filter_menu.place(x=160, y=3)
+
+# Label Edge Detection
+ed_label = tk.Label(frame, text="Edge Detection", bg="lightgrey", font=("Times New Roman", 10))
+ed_label.place(x=260,y=7)
+
+# Dropdown menu Edge Detection
+ed_var = tk.StringVar(value="None")
+ed_menu = tk.OptionMenu(frame, ed_var, "Sobel", "Prewitt", "Robert", "Canny")
+ed_menu.place(x=350, y=3)
+
+# Label Remove Background
+rm_label = tk.Label(frame, text="Remove BG", bg="lightgrey", font=("Times New Roman", 10))
+rm_label.place(x=450,y=7)
+
+# Dropdown menu Remove Background
+rm_var = tk.StringVar(value="Threshold")
+rm_menu = tk.OptionMenu(frame, rm_var, "50", "100", "150")
+rm_menu.place(x=520, y=3)
+
+# Label Image Compression 
+ic_label = tk.Label(frame, text="Compress", bg="lightgrey", font=("Times New Roman", 10))
+ic_label.place(x=620,y=7)
+
+# Dropdown menu Image Compression
+ic_var = tk.StringVar(value="None")
+ic_menu = tk.OptionMenu(frame, ic_var, "hihi")
+ic_menu.place(x=680, y=3)
+
+# Button Add Image
+add_button = tk.Button(frame, text="Add Image +", bg="grey", font=("Times New Roman", 10), command=open_image)
+add_button.place(x=760,y=5)
+
+# Create Work Frame
+work_frame = tk.Frame(frame, bd=1, relief="solid", bg="white", width=850, height=430)
+work_frame.place(x=0,y=35)
+
 # Create the left panel for the original image
-left_panel = tk.Frame(frame, width=400, height=500)
+left_panel = tk.Frame(work_frame, bd=1, relief="solid", width=425, height=430)
 left_panel.pack(side="left", fill="both", expand=True)
 
 # Create the right panel for the filtered image
-right_panel = tk.Frame(frame, width=400, height=500)
+right_panel = tk.Frame(work_frame, bd=1, relief="solid", width=425, height=430)
 right_panel.pack(side="right", fill="both", expand=True)
 
-# Original image section
-original_label = tk.Label(left_panel, text="Original Image", bg="white", font=("Arial", 14))
-original_label.pack(pady=10)
+# Label Image original
+original_label = tk.Label(left_panel, text="Original Image", bg="lightgrey", font=("Times New Roman", 10))
+original_label.place(x=170,y=0)
 
-# Filtered image section
-filtered_label = tk.Label(right_panel, text="Filtered Image", bg="white", font=("Arial", 14))
-filtered_label.pack(pady=10)
+# Label in Image
+i_label = tk.Label(left_panel, text="Input show here", font=Font(family="Times New Roman", size=10, slant="italic"))
+i_label.place(x=0,y=20)
 
-# Control buttons and dropdown
-control_frame = tk.Frame(root)
-control_frame.pack(pady=10)
+# Label Image edited 
+edited_label = tk.Label(right_panel, text="Edited Image", bg="lightgrey", font=("Times New Roman", 10))
+edited_label.place(x=170,y=0)
 
-# Button to open an image
-open_button = tk.Button(control_frame, text="Choose Image", command=open_image)
-open_button.pack(side="left", padx=10)
+# Label out Image
+o_label = tk.Label(right_panel, text="Output show here", font=Font(family="Times New Roman", size=10, slant="italic"))
+o_label.place(x=0,y=20)
 
-# Dropdown menu for selecting filters
-filter_var = tk.StringVar(value="Select Filter")
-filter_menu = tk.OptionMenu(control_frame, filter_var, "Grayscale", "Sepia", "Invert")
-filter_menu.pack(side="left", padx=10)
-
-# Button to apply the selected filter
-apply_button = tk.Button(control_frame, text="Apply Filter", command=apply_filter)
-apply_button.pack(side="left", padx=10)
-
-# Label to display status with the file name
-status_label = tk.Label(control_frame, text="No image loaded.", fg="blue")
-status_label.pack(side="left", padx=10)
+# Button Apply
+apply_button = tk.Button(frame, text="Apply", bg="grey", font=("Times New Roman", 10))
+apply_button.place(x=790,y=470)
 
 # Run the application
 root.mainloop()
